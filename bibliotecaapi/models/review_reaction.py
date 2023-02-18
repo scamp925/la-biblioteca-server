48752036363710228d98ee52cd19bcf94969fb4b
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-18 12:49:44 -0600

    Made new migrations and re-seeded the database

--- bibliotecaapi/models/review_reaction.py
+++ bibliotecaapi/models/review_reaction.py
@@ -2,9 +2,10 @@
 from django.db import models
 from .review import Review
 from .reaction import Reaction
-
+from .user import User
 
 class ReviewReaction(models.Model):
     '''ReviewReaction Class'''
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     reaction = models.ForeignKey(Reaction, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
