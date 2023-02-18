35437faaf9b3db97c3d32eb04ed27401abcaad98
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-18 12:51:58 -0600

    Created custom properties for reaction model

--- bibliotecaapi/models/reaction.py
+++ bibliotecaapi/models/reaction.py
@@ -6,3 +6,19 @@ class Reaction(models.Model):
     '''Reaction Class'''
     label = models.CharField(max_length=50)
     image_url = models.CharField(max_length=200)
+
+    @property
+    def reaction_clicked(self):
+        return self.__reaction_clicked
+    
+    @reaction_clicked.setter
+    def reaction_clicked(self, bool):
+        self.__reaction_clicked = bool
+        
+    @property
+    def reaction_count(self):
+        return self.__reaction_count
+    
+    @reaction_count.setter
+    def reaction_count(self, value):
+        self.__reaction_count = value
